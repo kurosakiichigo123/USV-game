891741ddf2411c0f395a5b62e4e750caa0b4fd70
Ecoboaea Denis <gennyeco2002wiko@gmail.com>
2024-05-06 18:25:47 +0300

    guywerfsdufduyyufsu

--- USVGame/USVGame/Content/Screens/Level1/exterior.tsx
+++ USVGame/USVGame/Content/Screens/Level1/exterior.tsx
@@ -271,4 +271,80 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="9273">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="15.9375" y="3.5625">
+    <polygon points="0,0 -3.125,1.0625 -5.25,2.4375 -7.9375,4.3125 -10.4375,6.5 -12,9.375 -12.375,11.6875 -12.4375,12.375 -4.5625,12.5 -2.6875,11.125 -0.625,9.75 0.125,9.5625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9274">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4.875" width="16" height="8.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="9275">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.125" y="3.3125">
+    <polygon points="0,0 3.5625,2 6.4375,4.25 9.6875,7.375 12.5625,10.4375 13.3125,12.625 3.1875,12.5 -0.1875,9.3125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9276">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="15.9375" y="12.375">
+    <polygon points="0,0 -2.5625,3.5625 -10.1875,3.5625 -9.25,0.6875 -7.625,-2.3125 -5.375,-4.25 -2.0625,-6.125 0,-6.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9277">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.0625" y="12.375">
+    <polygon points="0,0 1.1875,1.75 2.25,3.5625 9.9375,3.625 9.875,1.875 7.8125,-0.8125 6.1875,-2.8125 4.375,-4.8125 2.375,-5.75 0.25,-6.625 -0.0625,-6.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9449">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.6875" y="0" width="6.625" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="9451">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="0" width="7" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="9452">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="11.625" y="0.125">
+    <polygon points="0,0 1.5625,2.125 4.375,3.3125 4.375,15.3125 -0.5,13.75 -2.75,12.1875 -4.5625,10.1875 -5.625,7.75 -6.3125,4.5 -6.8125,1.25 -6.8125,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9453">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="3.9375" y="0.1875">
+    <polygon points="0,0 -0.4375,1.3125 -1.1875,2.25 -2.4375,3 -3.6875,3.3125 -4,3.125 -3.9375,14.25 -2.1875,14.25 1.375,13.75 3.5,12 5.4375,10.125 6.3125,7.1875 6.8125,3.875 7.5,1.4375 7.5625,0 7.5625,-0.1875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9625">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="3.75" y="-0.0625">
+    <polygon points="0,0 0.3125,2.5 1.5,5.375 2.25,8.25 4.9375,9.875 8.25,11.875 12.125,12.125 12.3125,6.625 10.5,6.0625 8.4375,4.75 7.0625,3 6,0.875 5.75,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9626">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="7.5" width="16" height="4.4375"/>
+  </objectgroup>
+ </tile>
+ <tile id="9627">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="13.0625" y="0">
+    <polygon points="0,0 0.0625,2.25 -1,4.75 -2.875,7.6875 -5.0625,9.75 -7.3125,11.125 -10.5,12.1875 -13.125,12.25 -13.0625,6.4375 -11.25,5.9375 -9.6875,5.125 -7.5,3.4375 -6.25,2.0625 -6.125,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
