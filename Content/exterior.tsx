6e73edf7e3f6e79be8be354269b6db55cd93c7eb
Nicolae Tugui <NicusorN5@proton.me>
2024-05-06 14:10:49 +0300

    q

---
+++ Content/exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="exterior" tilewidth="16" tileheight="16" tilecount="55792" columns="176">
+ <image source="Modern_Exteriors_Complete_Tileset.png" width="2816" height="5072"/>
+</tileset>
