b44dbf63232ad42079591a9d7efc209d0ea5bc5e
Nicolae Tugui <NicusorN5@proton.me>
2024-05-06 16:33:06 +0300

    player collision

---
+++ USVGame/USVGame/Content/Screens/Level1/exterior.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="exterior" tilewidth="16" tileheight="16" tilecount="55792" columns="176">
+ <image source="Modern_Exteriors_Complete_Tileset.png" width="2816" height="5072"/>
+ <tile id="1591">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="3" y="0" width="13" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1592">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="13" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1597">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1598">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1599">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
