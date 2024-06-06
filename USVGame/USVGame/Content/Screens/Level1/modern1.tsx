a44e7adbd9cd85780957dcd11c861963b8b9dfd2
Nicolae Tugui <NicusorN5@proton.me>
2024-06-06 20:54:16 +0300

    add modern office tileset 1

---
+++ USVGame/USVGame/Content/Screens/Level1/modern1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Modern_Office_MV_1_TILESETS_B-C-D-E" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
+ <image source="Modern_Office_MV_1_TILESETS_B-C-D-E.png" width="768" height="768"/>
+</tileset>
