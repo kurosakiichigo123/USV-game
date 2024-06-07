80ec23afa4e5124e1a0ce639e8ee0225fca67886
dovahkiin124 <ghidapeter@gmail.com>
2024-06-07 18:57:01 +0300

    Final commit
    Atat am putut sa fac

---
+++ USVGame/USVGame/Content/Screens/Level1/all.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="all" tilewidth="16" tileheight="16" tilecount="81152" columns="256">
+ <image source="CompleteMap.png" width="4096" height="5072"/>
+</tileset>
