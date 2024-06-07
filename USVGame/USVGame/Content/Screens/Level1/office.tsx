31d9a8018cc8a80e9631c24b74ccda1be8a768ad
dovahkiin124 <ghidapeter@gmail.com>
2024-06-07 15:52:26 +0300

    hvjh

---
+++ USVGame/USVGame/Content/Screens/Level1/office.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="office" tilewidth="16" tileheight="16" tilecount="848" columns="16">
+ <image source="../../../../../../../Modern Office/Modern_Office_Revamped_v1.1/Modern_Office_Revamped/Modern_Office_16x16.png" width="256" height="848"/>
+</tileset>
